2649528a5633aaca4be22ef5528c3676105ea566
kendach <domagoj.kenda@gmail.com>
2018-05-22 11:20:56 +0200

    exoplayer v 0.0.1

--- app/src/main/java/com/domagojkenda/onlineradio/MainActivity.java
+++ app/src/main/java/com/domagojkenda/onlineradio/MainActivity.java
@@ -2,11 +2,30 @@ package com.domagojkenda.onlineradio;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.io.IOException;
 
@@ -14,16 +33,50 @@ public class MainActivity extends AppCompatActivity {
 
     Button b_play;
 
-    MediaPlayer mediaPlayer;
+    ExoPlayer player;
 
     boolean prepared = false;
     boolean started = false;
 
-    String stream = "http://144.217.153.67/live?icy=http";
+    String stream = "http://161.53.122.184:8000/aacPlus48.aac";
+    private DataSource.Factory dataSourceFactory;
+    private ExtractorsFactory extractorsFactory;
+    // "http://144.217.153.67/live?icy=http";
+    // http://161.53.122.184:8000/aacPlus48.aac
 
 
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializePlayer();
+    }
+    */
 
+    private void initializePlayer() {
+        // Create a default TrackSelector
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
 
+        //Initialize the player
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+        //Initialize simpleExoPlayerView
+        /*
+        SimpleExoPlayerView simpleExoPlayerView = findViewById(R.id.exoplayer);
+        simpleExoPlayerView.setPlayer(player);
+        */
+        // Produces DataSource instances through which media data is loaded.
+        dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(
+                this, "RadioStudentExoplayer"));
+
+        // Produces Extractor instances for parsing the media data.
+        extractorsFactory = new DefaultExtractorsFactory();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +87,10 @@ public class MainActivity extends AppCompatActivity {
         b_play.setEnabled(false);
         b_play.setText("LOADING");
 
+        initializePlayer();
 
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
+        // mediaPlayer = new MediaPlayer();
+        // mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         new PlayerTask().execute(stream);
 
         b_play.setOnClickListener(new View.OnClickListener() {
@@ -45,31 +98,62 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(started){
                     started = false;
-                    mediaPlayer.pause();
+                    // mediaPlayer.pause();
+                    player.setPlayWhenReady(false);
                     b_play.setText("PLAY");
                 } else {
                     started = true;
-                    mediaPlayer.start();
+                    // mediaPlayer.start();
+                    player.setPlayWhenReady(true);
                     b_play.setText("PAUSE");
                 }
             }
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(started){
+            // mediaPlayer.pause();
+            player.setPlayWhenReady(true);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (started){
+            // mediaPlayer.start();
+            player.setPlayWhenReady(true);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (prepared){
+            // mediaPlayer.release();
+            player.setPlayWhenReady(false);
+        }
+    }
+
+    // * * * * * * *
 
     class PlayerTask extends AsyncTask<String, Void, Boolean>{
 
         @Override
         protected Boolean doInBackground(String... strings) {
+            //mediaPlayer.setDataSource(strings[0]);
+            // mediaPlayer.prepare();
+            // This is the MediaSource representing the media to be played.
+            Uri videoUri = Uri.parse(strings[0]);
+            MediaSource videoSource = new ExtractorMediaSource(videoUri,
+                    dataSourceFactory, extractorsFactory, null, null);
 
-            try {
-                mediaPlayer.setDataSource(strings[0]);
-                mediaPlayer.prepare();
-                prepared = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            // Prepare the player with the source.
+            player.prepare(videoSource);
+            prepared = true;
             return prepared;
         }
 
@@ -81,28 +165,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(started){
-            mediaPlayer.pause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (started){
-            mediaPlayer.start();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (prepared){
-            mediaPlayer.release();
-        }
-    }
 }
 
